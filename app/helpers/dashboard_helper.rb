78e7c5721bdb4029e1472605006a473d549e1403
Dimas <dimas@Fadzlis-MacBook-Pro.local>
2017-04-21 21:16:29 +0700

    countdown

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,4 +2,8 @@ module DashboardHelper
   def timeout_wording(t)
     distance_of_time_in_words(DateTime.now, (t.created_at + t.timeout.seconds))
   end
+
+  def timeout_countdown(t)
+    (t.created_at + t.timeout.seconds).strftime("%b, %d, %Y")
+  end
 end
