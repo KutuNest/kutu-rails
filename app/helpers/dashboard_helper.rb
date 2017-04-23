8043b14bc8692c05c85b3bc87a10bae880e65ae9
Dimas <dimas@Fadzlis-MacBook-Pro.local>
2017-04-23 23:47:25 +0700

    fix countdown

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,7 +3,13 @@ module DashboardHelper
     distance_of_time_in_words(Date.current.in_time_zone, (t.created_at + t.timeout.seconds))
   end
 
-  def timeout_countdown(t)
+  def timeout_countdown_old(t)
     (t.created_at + t.timeout.seconds).strftime("%b, %d, %Y")
   end
+
+  def timeout_countdown(t)
+    x = []
+    (t.created_at + t.timeout.seconds).strftime("%Y, %m, %d, %H, %M, %S, 000").split(",").each_with_index{|a,i| i == 1 ? x << (a.to_i-1) : x << a  }
+    x.join(",")
+  end  
 end
