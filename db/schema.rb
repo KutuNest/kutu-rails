ddc13eb9f8429a01b47db697894155c5aa928979
Dimas <dimas@Fadzlis-MacBook-Pro.local>
2017-04-24 18:36:39 +0700

    minor adjustments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411072804) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20170422063758) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "member_id"
@@ -30,9 +27,9 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.integer  "pool_id"
     t.integer  "pool_order"
     t.boolean  "has_finished"
-    t.index ["groupement_id"], name: "index_accounts_on_groupement_id", using: :btree
-    t.index ["member_id"], name: "index_accounts_on_member_id", using: :btree
-    t.index ["pool_id"], name: "index_accounts_on_pool_id", using: :btree
+    t.index ["groupement_id"], name: "index_accounts_on_groupement_id"
+    t.index ["member_id"], name: "index_accounts_on_member_id"
+    t.index ["pool_id"], name: "index_accounts_on_pool_id"
   end
 
   create_table "accounts_transactions", id: false, force: :cascade do |t|
@@ -45,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.integer  "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["country_id"], name: "index_banks_on_country_id", using: :btree
+    t.index ["country_id"], name: "index_banks_on_country_id"
   end
 
   create_table "countries", force: :cascade do |t|
@@ -64,7 +61,6 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "title"
-    t.integer  "default_member_id"
   end
 
   create_table "members", force: :cascade do |t|
@@ -82,8 +78,8 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -102,9 +98,9 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.integer  "groupement_id"
     t.string   "referral_code"
     t.string   "referrer_code"
-    t.index ["email"], name: "index_members_on_email", unique: true, using: :btree
-    t.index ["groupement_id"], name: "index_members_on_groupement_id", using: :btree
-    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["groupement_id"], name: "index_members_on_groupement_id"
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -120,8 +116,9 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "short_body"
-    t.index ["account_id"], name: "index_notifications_on_account_id", using: :btree
-    t.index ["transaction_id"], name: "index_notifications_on_transaction_id", using: :btree
+    t.string   "notification_event"
+    t.index ["account_id"], name: "index_notifications_on_account_id"
+    t.index ["transaction_id"], name: "index_notifications_on_transaction_id"
   end
 
   create_table "pools", force: :cascade do |t|
@@ -133,7 +130,7 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.integer  "timeout"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["groupement_id"], name: "index_pools_on_groupement_id", using: :btree
+    t.index ["groupement_id"], name: "index_pools_on_groupement_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -149,21 +146,9 @@ ActiveRecord::Schema.define(version: 20170411072804) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "sender_receipt"
-    t.integer  "member_id"
     t.boolean  "disputed"
     t.integer  "pool_id"
-    t.index ["member_id"], name: "index_transactions_on_member_id", using: :btree
-    t.index ["pool_id"], name: "index_transactions_on_pool_id", using: :btree
+    t.index ["pool_id"], name: "index_transactions_on_pool_id"
   end
 
-  add_foreign_key "accounts", "groupements"
-  add_foreign_key "accounts", "members"
-  add_foreign_key "accounts", "pools"
-  add_foreign_key "banks", "countries"
-  add_foreign_key "members", "groupements"
-  add_foreign_key "notifications", "accounts"
-  add_foreign_key "notifications", "transactions"
-  add_foreign_key "pools", "groupements"
-  add_foreign_key "transactions", "members"
-  add_foreign_key "transactions", "pools"
 end
